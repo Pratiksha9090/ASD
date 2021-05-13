5f0b88bc2f60a45b9e866859892c8a374430222d
ADMIN <ADMIN@DESKTOP-EUKOD12>
2021-05-13 11:16:50 +0530

    Hi [Pratiksha jagtap]

---
+++ src/StringPgrm.java
@@ -0,0 +1,10 @@
+
+public class StringPgrm {
+public static void main(String[] args) {
+	String str1="xyz";
+	String str2="India";
+	String str3=str1+str2;
+	String str4="xyzIndia";
+	System.out.println(str3==str4);
+}
+}
